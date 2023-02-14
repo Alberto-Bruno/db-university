cb2f6f78fbcd1a88b72b90a1f022e2674168e115
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-14 19:34:30 +0100

    Query con GROUP BY

--- db-uni/tracce SELECT & GROUP BY.sql
+++ db-uni/tracce SELECT & GROUP BY.sql
@@ -44,12 +44,21 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS 'Enrolled students', YEAR(`enrolment_date`) 
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT COUNT(*) AS 'teachers', `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id` AS `appeal`, AVG(`vote`) AS 'media'
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS 'Degree courses', department_id
+FROM `degrees`
+GROUP BY department_id;
